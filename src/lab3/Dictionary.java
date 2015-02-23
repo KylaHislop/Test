a17cfe7d5f602431425d068e34b6fbd88c2b168a
1305629 <1305629@RG43007.rgu.ac.uk>
2015-02-23 09:41:17 +0000

    Program to act as dictionary by getting definition of a word

---
+++ src/lab3/Dictionary.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3;
+
+/**
+ *
+ * @author 1305629
+ * @version 23/02/2015
+ */
+public interface Dictionary {
+
+    void getDefinition(String word);
+
+}
